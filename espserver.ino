1f8b05e327c9f745dcc2466cfbcbc865a26c33a2
Timothy J Low <low8@ecegrid-thin1.ecn.purdue.edu>
2022-11-11 11:36:25 -0500

    Small updates. Still needs testing.

--- espserver.ino
+++ espserver.ino
@@ -1,7 +1,7 @@
 #include <WiFi.h>
 
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid = "Delta 1 Niner";
+const char* password = "mitchdaniels22";
 
 WiFiServer server(80);
 
